ea5816a414a4beb2e0c6ac5e7d24b6981308d504
kchyzhyk <kchyzhykova888@gmail.com>
2025-04-23 22:59:06 +0200

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,21 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "profimakac.cz",
+          },
+        ],
+        destination: "https://profimakac.cz/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
