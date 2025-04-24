58e07793fe6ec32870a24bbe2788cbc37471fab9
kchyzhyk <kchyzhykova888@gmail.com>
2025-04-24 16:53:24 +0200

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,10 @@ const nextConfig = {
             key: "Strict-Transport-Security",
             value: "max-age=63072000; includeSubDomains; preload",
           },
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
         ],
       },
     ];
